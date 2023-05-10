56019bd99797b17d7d972d9130c61a67e030ac1e
AmilEyvazov <127213330+amileyvazov@users.noreply.github.com>
2023-05-10 17:28:09 +0400

    Add files via upload

---
+++ claasstask/assets/js/main.js
@@ -0,0 +1,17 @@
+// const em=document.getElementById('email');
+// const pas=document.getElementById('password');
+
+// document.button.addEventListener("keyup",(e)=>{
+//     let but=document.getElementsByClassName('.click');
+//     if(em.include('@')) {
+//         em.style.borderColor='red'
+   
+//     }else if(pas.length <= 6){
+//       pas.style.borderColor='red'
+//     }
+   
+ 
+// });
+// document.button.addEventListener(em,pas)
+
+
